e7617af44a6d31c2617d94062f4b63d2d70fbbc4
Artem Logvinov <artemartem330@yandex.ru>
2024-05-21 19:15:54 +0300

    Practice 19

---
+++ practice19/src/main/java/org/example/practice19/Service.java
@@ -0,0 +1,51 @@
+package org.example.practice19;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.practice19.entities.Feedback;
+import org.example.practice19.entities.Product;
+import org.example.practice19.repo.FeedbackRepository;
+import org.example.practice19.repo.ProductRepository;
+
+@org.springframework.stereotype.Service
+@AllArgsConstructor
+@Slf4j
+public class Service {
+    private ProductRepository productRepository;
+    private FeedbackRepository feedbackRepository;
+
+    public Iterable<Product> getProducts() {
+        log.info("!!!  All products  !!!");
+        return productRepository.findAll();
+    }
+
+    public Product addProduct(Product product) {
+        log.info("!!!  Add product  !!!");
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id) {
+        log.info("!!!  Delete product  !!!");
+        productRepository.deleteById(id);
+    }
+
+    public Iterable<Feedback> getFeedbacks() {
+        log.info("!!!  All feedbacks  !!!");
+        return feedbackRepository.findAll();
+    }
+
+    public Feedback addFeedback(Feedback feedback) {
+        log.info("!!!  Add feedback  !!!");
+        return feedbackRepository.save(feedback);
+    }
+
+    public void deleteFeedback(long id) {
+        log.info("!!!  Delete feedback  !!!");
+        feedbackRepository.deleteById(id);
+    }
+
+    public Iterable<Feedback> getSpecificFeedback(String name) {
+        log.info("!!!  Get feedback by name  !!!");
+        return feedbackRepository.findAllByName(name);
+    }
+}
---
+++ practice19/src/main/java/org/example/practice19/repo/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.example.practice19.repo;
+
+import org.example.practice19.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
